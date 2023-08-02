5710594419b5b8594648015a47ba61d5949b6716
NghiaPM <minhnghia.pham.it@gmail.com>
2023-08-02 11:19:29 +0700

    add exception handle rd kafka

--- app/Http/Controllers/RDKafKaController.php
+++ app/Http/Controllers/RDKafKaController.php
@@ -6,24 +6,23 @@ namespace App\Http\Controllers;
 
 class RDKafKaController extends Controller
 {
-    //fix https://github.com/php-enqueue/enqueue-dev/issues/749
-    public function sendRDKafka(): bool
+    /**
+     * fix https://github.com/php-enqueue/enqueue-dev/issues/749
+     * $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($dataPushFeed));
+     * fc produce() php rd kafka return void and no ex, dont have way detect produce error or success
+     * this is solution:
+     * 1)  It is advised to call poll at regular intervals to serve callbacks. In php-rdkafka:3.x
+     * poll was also called during shutdown, so not calling it in regular intervals might
+     * lead to a slightly longer shutdown. The example below polls until there are no more events in the queue:
+     *
+     * $producer->produce(...);
+     * while ($producer->getOutQLen() > 0) {
+     * $producer->poll(1);
+     * }
+     * 2) Add solution timeout
+     */
+    public function sendRDKafkaSolution1(): bool
     {
-        /**
-         * https://github.com/php-enqueue/enqueue-dev/issues/749
-         * $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($dataPushFeed));
-         * fc produce() php rd kafka return void and no ex, dont have way detect produce error or success
-         * this is solution:
-         * 1)  It is advised to call poll at regular intervals to serve callbacks. In php-rdkafka:3.x
-         * poll was also called during shutdown, so not calling it in regular intervals might
-         * lead to a slightly longer shutdown. The example below polls until there are no more events in the queue:
-         *
-         * $producer->produce(...);
-         * while ($producer->getOutQLen() > 0) {
-         * $producer->poll(1);
-         * }
-         * 2) Add solution timeout
-         */
         $brokers = "test";
         $topic = "topic";
         $conf = new \RdKafka\Conf();
@@ -32,13 +31,6 @@ class RDKafKaController extends Controller
         $producer->addBrokers($brokers);
         $topic = $producer->newTopic($topic);
 
-        $dataPushFeed = [
-            'session_id' => 12123131,
-            'shop_code' => "TEST",
-            'shop_id' => 1,
-            'session_status' => "test",
-            'event' => Constant::BBS_SESSION_EVENT['UPDATE'],
-        ];
         dump($producer->getOutQLen());
         $topic->produce(RD_KAFKA_PARTITION_UA, 0, "test messsage");
 
@@ -61,4 +53,29 @@ class RDKafKaController extends Controller
 
         return true;
     }
+
+    // return ex
+    public function sendRDKafkaSolution2()
+    {
+        $brokers = "test";
+        $topic = "topic";
+        $conf = new \RdKafka\Conf();
+        $conf->set('debug','all');
+        $producer = new \RdKafka\Producer($conf);
+        $producer->addBrokers($brokers);
+        $topic = $producer->newTopic($topic);
+
+        dump($producer->getOutQLen());
+        $topic->produce(RD_KAFKA_PARTITION_UA, 0, "test messsage");
+
+        $start = microtime(true);
+        while ($producer->getOutQLen() > 0) {
+            $topic->poll(1);
+
+            if (microtime(true) - $start > 10) {
+                throw new \RuntimeException("Message sending failed");
+            }
+        }
+    }
+
 }
